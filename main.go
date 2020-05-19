dc10da70774393db1ed6a75718d3bd84bac001e8
Airton F. S. Oliveira <airtonfelipe@gmail.com>
2020-05-19 11:50:19 -0300

    Exemplo de receiver

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func receive(msgs <-chan amqp.Delivery) {
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Falha ao conectar")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Falha ao abrir canal")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", //name
+		false,   //durable
+		false,   //delete when unused
+		false,   //exclusive
+		false,   //no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Falha ao declarar fila")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Falha ao registrar consumidor")
+
+	//Pendurar e aguardar mensagens
+	receive(msgs)
+}
